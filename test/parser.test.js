c56a38b153f34ade135d5d1a3a601e40b3be59f8
Nicholas <nickkasten03@gmail.com>
2025-02-21 10:56:23 -0800

    Adding 3 model programs, and 1 syntactically correct and 1 syntactically wrong test case that both pass

---
+++ test/parser.test.js
@@ -0,0 +1,26 @@
+import { describe, it } from "node:test"
+import assert from "node:assert/strict"
+import parse from "../src/parser.js"
+
+// Expected to be syntactically correct
+const syntaxChecks = [
+    ["simplest syntactically correct program", "main = 0"]
+]
+
+// Expected to be syntactically incorrect
+const syntaxErrors = [
+    ["misplaced semicolon", "main = 0;", /Line 1, col 9/],
+]
+
+describe("Parser", () => {
+    for (const [scenario, source] of syntaxChecks) {
+        it(`Matches ${scenario}`, () => {
+            assert(parse(source).succeeded())
+        })
+    }
+    for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+        it(`throws on ${scenario}`, () => {
+            assert.throws(() => parse(source), errorMessagePattern)
+        })
+    }
+})
