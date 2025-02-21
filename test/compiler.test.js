48930ef1bd510c488c0afb244a5320c86dd00bd2
Eren Unsal <aaronunsal@gmail.com>
2025-02-20 22:44:11 -0800

    Switched to node testing with c8 framework

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,3 +1,8 @@
-test('Is one equal to one?!?!', () => {
-  expect(1).toBe(1);
+import { describe, it } from 'node:test';
+import { deepEqual } from 'node:assert/strict';
+
+describe('Compiler', () => {
+  it('Is one equal one?!', () => {
+    deepEqual(1, 1);
+  });
 });
