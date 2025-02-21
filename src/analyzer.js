cc0bb6b4cd25cd18c92f1ff8dbd5e8cf4d77df55
Eren Unsal <aaronunsal@gmail.com>
2025-02-20 22:48:03 -0800

    Added stubs to the analyzer, generator, optimizer, and parser.

---
+++ src/analyzer.js
@@ -0,0 +1,3 @@
+export default function analyze(sourceCode) {
+    throw new Error("Not implemented!")
+}
---
+++ src/optimizer.js
@@ -0,0 +1,3 @@
+export default function optimize(sourceCode) {
+    throw new Error("Not implemented!")
+}
