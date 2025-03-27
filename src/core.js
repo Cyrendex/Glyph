0db635f86328df0719ac14279cead338ddd90b9a
Nicholas <nickkasten03@gmail.com>
2025-03-27 00:36:52 -0700

    Adding the work for the core and updating the ohm

--- src/core.js
+++ src/core.js
@@ -1,169 +1,153 @@
 export function program(statements) {
     return { kind: "Program", statements }
+}
+  
+export function variableDeclaration(variable, initializer) {
+  return { kind: "VariableDeclaration", variable, initializer }
+}
+
+export function variable(name, type) {
+  return { kind: "Variable", name, type }
+}
+
+export const intType = "int"
+export const floatType = "float"
+export const decimType = "decim"
+export const slashType = "slash"
+export const slogType = "slog"
+export const booleanType = "bool"
+export const stringType = "string"
+export const codepointType = "codepoint"
+export const voidType = "void"
+export const anyType = "any"
+export const glyphType = "glyph"
+
+export function mainStatement(executables) {
+  return { kind: "MainStatement", executables }
+}
+
+export function conjureStatement(block) {
+  return { kind: "ConjureStatement", block }
+}
+
+export function importStatement(module, imports) {
+  return { kind: "ImportStatement", module, imports }
+}
+
+export function whileStatement(condition, block) {
+  return { kind: "WhileStatement", condition, block }
+}
+
+export function ifStatement(condition, consequent, alternative) {
+  return { kind: "IfStatement", condition, consequent, alternative}
+}
+
+// export function shortIfStatement(condition, consequent) {
+//   return { kind: "ShortIfStatement", condition, consequent }
+// }
+
+export function lambdaDeclaration(lambda, initializer) {
+  return { kind: "LambdaStatement", lambda, initializer }
+}
+
+export function lambda(name, params, body, type) {
+  return { kind: "Lambda", name, params, body, type}
+}
+
+export function functionDeclaration(fun) {
+  return { kind: "FunctionEvoke", fun }
+}
+
+export function fun(name, parameters, body, returnHint) {
+  return { kind: "Function", name, parameters, body, returnHint }
+}
+
+export function importedFunction(module, name, type) {
+  return { kind: "Function", module, name, type, imported: true }
+}
+
+export function functionType(paramTypes, returnTypes) {
+  return { kind: "FunctionType", paramTypes, returnTypes}
+}
+
+export function optionalType(baseType) {
+  return { kind: "OptionalType", baseType}
+}
+
+export function returnStatement(expression) {
+  return { kind: "ReturnStatement", expression }
+}
+
+export function increment(variable) {
+  return { kind: "Increment", variable }
+}
+
+export function decrement(variable) {
+  return { kind: "Decrement", variable }
+}
+
+export function assignment(target, source) {
+  return { kind: "Assignment", target, source}
+}
+
+export const breakStatement = { kind: "BreakStatement" }
+
+export function binary(op, left, right, type) {
+  return { kind: "BinaryExpression", op, left, right, type}
+}
+
+export function unary(op, operand, type) {
+  return { kind: "UnaryExpression", op, operand, type}
+}
+
+export function subscript(array, index) {
+  return { kind: "SubscriptExpression", array, index}
+}
+
+export function arrayExpression(elements) {
+  return { kind: "ArrayExpression", elements}
+}
+
+export function functionCall(callee, args) {
+  if (callee.imported) {
+    // NMK: idk smth that makes it act normal
   }
-  
-  export function variableDeclaration(variable, initializer) {
-    return { kind: "VariableDeclaration", variable, initializer }
-  }
-  
-  export function variable(name, readOnly, type) {
-    return { kind: "Variable", name, readOnly, type }
-  }
-  
-  export function typeDeclaration(type) {
-    return { kind: "TypeDeclaration", type }
-  }
-  
-  export const boolType = { kind: "BoolType" }
-  export const intType = { kind: "IntType" }
-  export const floatType = { kind: "FloatType" }
-  export const stringType = { kind: "StringType" }
-  export const voidType = { kind: "VoidType" }
-  export const anyType = { kind: "AnyType" }
-  
-  export function structType(name, fields) {
-    return { kind: "StructType", name, fields }
-  }
-  
-  export function field(name, type) {
-    return { kind: "Field", name, type }
-  }
-  
-  export function functionDeclaration(fun, params, body) {
-    return { kind: "FunctionDeclaration", fun, params, body }
-  }
-  
-  export function fun(name, type) {
-    return { kind: "Function", name, type }
-  }
-  
-  export function arrayType(baseType) {
-    return { kind: "ArrayType", baseType }
-  }
-  
-  export function functionType(paramTypes, returnType) {
-    return { kind: "FunctionType", paramTypes, returnType }
-  }
-  
-  export function optionalType(baseType) {
-    return { kind: "OptionalType", baseType }
-  }
-  
-  export function increment(variable) {
-    return { kind: "Increment", variable }
-  }
-  
-  export function decrement(variable) {
-    return { kind: "Decrement", variable }
-  }
-  
-  export function assignment(target, source) {
-    return { kind: "Assignment", target, source }
-  }
-  
-  export const breakStatement = { kind: "BreakStatement" }
-  
-  export function returnStatement(expression) {
-    return { kind: "ReturnStatement", expression }
-  }
-  
-  export function shortReturnStatement() {
-    return { kind: "ShortReturnStatement" }
-  }
-  
-  export function ifStatement(test, consequent, alternate) {
-    return { kind: "IfStatement", test, consequent, alternate }
-  }
-  
-  export function shortIfStatement(test, consequent) {
-    return { kind: "ShortIfStatement", test, consequent }
-  }
-  
-  export function whileStatement(test, body) {
-    return { kind: "WhileStatement", test, body }
-  }
-  
-  export function repeatStatement(count, body) {
-    return { kind: "RepeatStatement", count, body }
-  }
-  
-  export function forRangeStatement(iterator, low, op, high, body) {
-    return { kind: "ForRangeStatement", iterator, low, op, high, body }
-  }
-  
-  export function forStatement(iterator, collection, body) {
-    return { kind: "ForStatement", iterator, collection, body }
-  }
-  
-  export function conditional(test, consequent, alternate, type) {
-    return { kind: "Conditional", test, consequent, alternate, type }
-  }
-  
-  export function binary(op, left, right, type) {
-    return { kind: "BinaryExpression", op, left, right, type }
-  }
-  
-  export function unary(op, operand, type) {
-    return { kind: "UnaryExpression", op, operand, type }
-  }
-  
-  export function emptyOptional(baseType) {
-    return { kind: "EmptyOptional", baseType, type: optionalType(baseType) }
-  }
-  
-  export function subscript(array, index) {
-    return { kind: "SubscriptExpression", array, index, type: array.type.baseType }
-  }
-  
-  export function arrayExpression(elements) {
-    return { kind: "ArrayExpression", elements, type: arrayType(elements[0].type) }
-  }
-  
-  export function emptyArray(type) {
-    return { kind: "EmptyArray", type }
-  }
-  
-  export function memberExpression(object, op, field) {
-    return { kind: "MemberExpression", object, op, field, type: field.type }
-  }
-  
-  export function functionCall(callee, args) {
-    return { kind: "FunctionCall", callee, args, type: callee.type.returnType }
-  }
-  
-  export function constructorCall(callee, args) {
-    return { kind: "ConstructorCall", callee, args, type: callee }
-  }
-  
-  // These local constants are used to simplify the standard library definitions.
-  const floatToFloatType = functionType([floatType], floatType)
-  const floatFloatToFloatType = functionType([floatType, floatType], floatType)
-  const stringToIntsType = functionType([stringType], arrayType(intType))
-  const anyToVoidType = functionType([anyType], voidType)
-  
-  export const standardLibrary = Object.freeze({
-    int: intType,
-    float: floatType,
-    boolean: boolType,
-    string: stringType,
-    void: voidType,
-    any: anyType,
-    π: variable("π", true, floatType),
-    print: fun("print", anyToVoidType),
-    sin: fun("sin", floatToFloatType),
-    cos: fun("cos", floatToFloatType),
-    exp: fun("exp", floatToFloatType),
-    ln: fun("ln", floatToFloatType),
-    hypot: fun("hypot", floatFloatToFloatType),
-    bytes: fun("bytes", stringToIntsType),
-    codepoints: fun("codepoints", stringToIntsType),
-  })
-  
-  // We want every expression to have a type property. But we aren't creating
-  // special entities for numbers, strings, and booleans; instead, we are
-  // just using JavaScript values for those. Fortunately we can monkey patch
-  // the JS classes for these to give us what we want.
-  String.prototype.type = stringType
-  Number.prototype.type = floatType
-  BigInt.prototype.type = intType
-  Boolean.prototype.type = boolType
+  return { kind: "FunctionCall", callee, args, type: callee.type.returnTypes}
+}
+
+// NMK: Don't know if we actually need these, Toal's suggestion for his lang
+// // These local constants are used to simplify the standard library definitions.
+// const floatToFloatType = functionType([floatType], floatType)
+// const floatFloatToFloatType = functionType([floatType, floatType], floatType)
+// const stringToIntsType = functionType([stringType], arrayType(intType))
+// const anyToVoidType = functionType([anyType], voidType)
+
+// NMK: Need to modify this to support our tomes (modules)
+// export const standardLibrary = Object.freeze({
+//   int: intType,
+//   float: floatType,
+//   boolean: boolType,
+//   string: stringType,
+//   void: voidType,
+//   any: anyType,
+//   π: variable("π", true, floatType),
+//   print: fun("print", anyToVoidType),
+//   sin: fun("sin", floatToFloatType),
+//   cos: fun("cos", floatToFloatType),
+//   exp: fun("exp", floatToFloatType),
+//   ln: fun("ln", floatToFloatType),
+//   hypot: fun("hypot", floatFloatToFloatType),
+//   bytes: fun("bytes", stringToIntsType),
+//   codepoints: fun("codepoints", stringToIntsType),
+// })
+
+
+// NMK: Again, Don't know if we need Toal's monkey patching but wdik
+// We want every expression to have a type property. But we aren't creating
+// special entities for numbers, strings, and booleans; instead, we are
+// just using JavaScript values for those. Fortunately we can monkey patch
+// the JS classes for these to give us what we want.
+String.prototype.type = stringType
+Number.prototype.type = floatType
+BigInt.prototype.type = intType
+Boolean.prototype.type = boolType
