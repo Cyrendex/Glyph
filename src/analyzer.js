2881b1ffa0bd35e6db8beb9e267ecbd0f174fdc1
Mehmet Colakoglu <mehmetnuri.colakoglu@gmail.com>
2025-03-29 20:25:40 -0700

    most functions

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,102 +1,352 @@
-import * as core from "./core.js"
+import * as core from "./core.js";
 
 class Context {
-    constructor({ parent = null, locals = new Map(), inLoop = false, function: f = null }) {
-        Object.assign(this, { parent, locals, inLoop, function: f })
-    }
+  constructor({
+    parent = null,
+    locals = new Map(),
+    inLoop = false,
+    function: f = null,
+  }) {
+    Object.assign(this, { parent, locals, inLoop, function: f });
+  }
 
-    add(name, entity) {
-        this.locals.set(name, entity)
-    }
+  add(name, entity) {
+    this.locals.set(name, entity);
+  }
 
-    lookup(name) {
-        return this.locals.get(name) || this.parent?.lookup(name)
-    }
+  lookup(name) {
+    return this.locals.get(name) || this.parent?.lookup(name);
+  }
 
-    static root() {
-        return new Context({ locals: new Map(Object.entries(core.standardLibrary)) })
-    }
+  static root() {
+    return new Context({
+      locals: new Map(Object.entries(core.standardLibrary)),
+    });
+  }
 
-    newChildContext(props) {
-        return new Context({ ...this, ...props, parent: this, locals: new Map() })
-    }
+  newChildContext(props) {
+    return new Context({ ...this, ...props, parent: this, locals: new Map() });
+  }
 }
 
 export default function analyze(match) {
-    let context = Context.root()
+  let context = Context.root();
 
-    function check(condition, message, errorLocation) {
-        if (!condition) {
-            const prefix = errorLocation.at.source.getLineAndColumnMessage()
-            throw new Error(`${prefix}${message}`)
-        }
+  function check(condition, message, errorLocation) {
+    if (!condition) {
+      const prefix = errorLocation.at.source.getLineAndColumnMessage();
+      throw new Error(`${prefix}${message}`);
     }
-    
-    function checkNotAlreadyDeclared(name, source) {
-        check(!context.lookup(name), `Identifier ${name} already declared`, source)
-    }
-    
-    function checkNotDeclared(entity, name, source) {
-        check(entity, `Identifier ${name} not declared`, source)
-    }
+  }
 
-    function checkIsInteger(e, source) {
-        check(e.type === core.intType, "Expected an integer", source)
-    }
+  function checkNotAlreadyDeclared(name, source) {
+    check(!context.lookup(name), `Identifier ${name} already declared`, source);
+  }
 
-    function checkIsFloat(e, source) {
-        check(e.type === core.floatType, "Expected a float", source)
-    }
+  function checkNotDeclared(entity, name, source) {
+    check(entity, `Identifier ${name} not declared`, source);
+  }
 
-    function checkIsDecimal(e, source) {
-        check(e.type === core.decimType, "Expected a decimal", source)
-    }
+  function checkIsInteger(e, source) {
+    check(e.type === core.intType, "Expected an integer", source);
+    return Number.isInteger(e);
+  }
 
-    function checkIsSlash(e, source) {
-        check(e.type === core.slashType, "Expected a slash", source)
-    }
+  function checkIsFloat(e, source) {
+    check(e.type === core.floatType, "Expected a float", source);
+    num.toString().includes(".");
+  }
 
-    function checkIsBoolean(e, source) {
-        check(e.type === core.booleanType, "Expected a boolean", source)
-    }
+  function checkIsDecimal(e, source) {
+    check(e.type === core.decimType, "Expected a decimal", source);
+    return Number(num) === num && num % 1 !== 0;
+  }
 
-    function checkIsGlyph(e, source) {
-        check(e.type === core.glyphType, "Expected a glyph", source)
-    }
+  function checkIsSlash(e, source) {
+    check(e.type === core.slashType, "Expected a slash", source);
+  }
 
-    function checkIsString(e, source) {
-        check(e.type === core.stringType, "Expected a string", source)
-    }
+  function checkIsSlog(e, source) {
+    check(e.type === core.slogType, "Expected a slog", source);
+  }
 
-    function checkIsCodepoint(e, source) {
-        check(e.type === core.codepointType, "Expected a codepoint", source)
-    }
+  function checkIsBoolean(e, source) {
+    check(e.type === core.booleanType, "Expected a boolean", source);
+    return typeof (e, Boolean);
+  }
 
-    function checkIsVoid(e, source) {
-        check(e.type === core.voidType, "Expected void", source)
-    }
+  function checkIsGlyph(e, source) {
+    check(e.type === core.glyphType, "Expected a glyph", source);
+  }
 
-    function checkIsAny(e, source) {
-        check(e.type === core.anyType, "Expected any", source)
-    }
+  function checkIsString(e, source) {
+    check(e.type === core.stringType, "Expected a string", source);
+  }
 
-    function checkIsNumeric(e, source) {
-        const expectedTypes = [core.intType, core.floatType, core.decimType, core.slashType, core.slogType]
-        check(expectedTypes.includes(e.type), "Expected a number", source)
-    }
+  function checkIsCodepoint(e, source) {
+    check(e.type === core.codepointType, "Expected a codepoint", source);
+  }
 
-    function checkIsText(e, source) {
-        const expectedTypes = [core.stringType, core.glyphType]
-        check(expectedTypes.includes(e.type), "Expected a glyph or string", source)
-    }
+  function checkIsVoid(e, source) {
+    check(e.type === core.voidType, "Expected void", source);
+  }
 
-    function checkIsNumericOrText(e, source) {
-        const expectedTypes = [core.intType, core.floatType, core.decimType, core.slashType, core.slogType, core.stringType, core.glyphType]
-        check(expectedTypes.includes(e.type), "Expected a number or string", source)
-    }
+  function checkIsAny(e, source) {
+    check(e.type === core.anyType, "Expected any", source);
+  }
 
-    function checkIsAType(e, source) {
-        const isBasicType = /int|float|decim|slash|slog|bool|string|glyph|codepoint|void|any/.test(e)
-        check(isBasicType, "Type expected", source)
-    }
+  function checkIsNumeric(e, source) {
+    const expectedTypes = [
+      core.intType,
+      core.floatType,
+      core.decimType,
+      core.slashType,
+      core.slogType,
+    ];
+    check(expectedTypes.includes(e.type), "Expected a number", source);
+  }
+
+  function checkIsText(e, source) {
+    const expectedTypes = [core.stringType, core.glyphType];
+    check(expectedTypes.includes(e.type), "Expected a glyph or string", source);
+  }
+
+  function checkIsNumericOrText(e, source) {
+    const expectedTypes = [
+      core.intType,
+      core.floatType,
+      core.decimType,
+      core.slashType,
+      core.slogType,
+      core.stringType,
+      core.glyphType,
+    ];
+    check(
+      expectedTypes.includes(e.type),
+      "Expected a number or string",
+      source
+    );
+  }
+
+  function checkIsAType(e, source) {
+    const isBasicType =
+      /int|float|decim|slash|slog|bool|string|glyph|codepoint|void|any/.test(e);
+    check(isBasicType, "Type expected", source);
+  }
+
+  //After this point is Mehmet
+
+  //Toal has this but we don't
+  function checkBothHaveTheSameType() {}
+
+  //Toal has this but we don't
+  function checkAllHaveSameType() {}
+
+  //Toal has this but we don't
+  function includesAsField() {}
+
+  //Toal has this but we don't
+  function checkNotBeSelfContaining() {}
+
+  //Toal has this but we don't
+  function equivalent() {}
+
+  //Toal has this but we don't
+  function assignable() {}
+
+  //Toal has this but we don't
+  function typeDescription() {}
+
+  //Toal has this but we don't
+  function checkBeAssignable() {}
+
+  //Toal has this but we don't
+  function checkHaveDistinctFields() {}
+
+  //Toal has this but we don't
+  function checkHaveMember() {}
+
+  //Toal has this but we don't
+  function checkBeInLoop() {}
+
+  //Toal has this but we don't
+  function checkBeInAFunction() {}
+
+  //Toal has this but we don't
+  function checkBeCallable() {}
+
+  //Toal has this but we don't
+  function checkNotReturnAnything() {}
+
+  //Toal has this but we don't
+  function checkReturnSomething() {}
+
+  //Toal has this but we don't
+  function checkBeReturnable() {}
+
+  //Toal has this but we don't
+  function checkHaveCorrectArgumentCount() {}
+
+  //builder goes here
+  const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
+    Program(statements) {
+      return core.program(statements.children.map((s) => s.rep()));
+    },
+    //this nxt line needs to be changed to include all the functions that come after
+    //and all functions must be "unfunctioned"
+  });
+
+  //we have this but Toal doesn't
+  function mainStatement() {}
+
+  //we have this but Toal doesn't
+  function exscribeStatement() {}
+
+  //we have this but Toal doesn't
+  function conjureStatement() {}
+
+  //we have this but Toal doesn't
+  function importStatement() {}
+
+  //we have this but Toal doesn't
+  function lambdaDeclaration() {}
+
+  //aren't these same as conjure
+  //we have this but Toal doesn't
+  function lambda() {}
+
+  //aren't these same as conjure
+  //we have this but Toal doesn't
+  function importedFunction() {}
+
+  //Toal has this but we don't
+  function variableDecl() {}
+
+  //Toal has this but we don't
+  function typeDecl() {}
+
+  //Toal has this but we don't
+  function field() {}
+
+  function FunDecl() {}
+
+  function Params() {}
+
+  function Param() {}
+
+  function Type_optional() {}
+
+  //Toal has this but we don't
+  function Type_function() {}
+
+  function Type_id() {}
+
+  function Statement_bump() {}
+
+  function Statement_assign() {}
+
+  //Toal has this but we don't
+  function Statement_call() {}
+
+  function Statement_break() {}
+
+  function Statement_return() {}
+
+  //Toal has this but we don't
+  function Statement_shortreturn() {}
+
+  function IfStmt_long() {}
+
+  //Toal has this but we don't
+  function IfStmt_elsif() {}
+
+  //Toal has this but we don't
+  function IfStmt_short() {}
+
+  function LoopStmt_while() {}
+
+  //Toal has this but we don't
+  function LoopStmt_repeat() {}
+
+  //Toal has this but we don't
+  function LoopStmt_range() {}
+
+  //Toal has this but we don't
+  function LoopStmt_collection() {}
+
+  //Toal has this but we don't
+  function Block() {}
+
+  //Toal has this but we don't
+  function Exp_conditional() {}
+
+  //Toal has this but we don't
+  function Exp_unwrapelse() {}
+
+  function Exp_or() {}
+
+  function Exp_and() {}
+
+  function Exp_xor() {}
+
+  function Exp_bitor() {}
+
+  function Exp_bitand() {}
+
+  function Exp_bitxor() {}
+
+  //Toal has this but we don't
+  function Exp_compare() {}
+
+  //Toal has this but we don't
+  function Exp_shift() {}
+
+  //Toal has this but we don't
+  function Exp_add() {}
+
+  //Toal has this but we don't
+  function Exp_multiply() {}
+
+  //Toal has this but we don't
+  function Exp_power() {}
+
+  function Exp_unary() {}
+
+  //Toal has this but we don't
+  function Exp_emptyarray() {}
+
+  //Toal has this but we don't
+  function Exp_arrayexpression() {}
+
+  //Toal has this but we don't
+  function Exp_arrayoptional() {}
+
+  //Toal has this but we don't
+  function Exp_parens() {}
+
+  //Toal has this but we don't
+  function Exp_subscript() {}
+
+  //Toal has this but we don't
+  function Exp_member() {}
+
+  //Toal has this but we don't
+  function Exp_call() {}
+
+  //Toal has this but we don't
+  function Exp_id() {}
+
+  function ttrue() {}
+
+  function ffalse() {}
+
+  function floatlit() {}
+
+  function stringlit() {}
+
+  //change the return to the correct build
+  return {
+    kind: "Program",
+    body: { kind: "potato" },
+  };
 }
--- src/core.js
+++ src/core.js
@@ -1,45 +1,49 @@
 export function program(statements) {
-    return { kind: "Program", statements }
+  return { kind: "Program", statements };
 }
-  
+
 export function variableDeclaration(variable, initializer) {
-  return { kind: "VariableDeclaration", variable, initializer }
+  return { kind: "VariableDeclaration", variable, initializer };
 }
 
 export function variable(name, type) {
-  return { kind: "Variable", name, type }
+  return { kind: "Variable", name, type };
 }
 
-export const intType = "int"
-export const floatType = "float"
-export const decimType = "decim"
-export const slashType = "slash"
-export const slogType = "slog"
-export const booleanType = "bool"
-export const stringType = "string"
-export const codepointType = "codepoint"
-export const voidType = "void"
-export const anyType = "any"
-export const glyphType = "glyph"
+export const intType = "int";
+export const floatType = "float";
+export const decimType = "decim";
+export const slashType = "slash";
+export const slogType = "slog";
+export const booleanType = "bool";
+export const stringType = "string";
+export const codepointType = "codepoint";
+export const voidType = "void";
+export const anyType = "any";
+export const glyphType = "glyph";
 
 export function mainStatement(executables) {
-  return { kind: "MainStatement", executables }
+  return { kind: "MainStatement", executables };
+}
+
+export function exscribeStatement(expression) {
+  return { kind: "ExscribeStatement", expression };
 }
 
 export function conjureStatement(block) {
-  return { kind: "ConjureStatement", block }
+  return { kind: "ConjureStatement", block };
 }
 
 export function importStatement(module, imports) {
-  return { kind: "ImportStatement", module, imports }
+  return { kind: "ImportStatement", module, imports };
 }
 
 export function whileStatement(condition, block) {
-  return { kind: "WhileStatement", condition, block }
+  return { kind: "WhileStatement", condition, block };
 }
 
 export function ifStatement(condition, consequent, alternative) {
-  return { kind: "IfStatement", condition, consequent, alternative}
+  return { kind: "IfStatement", condition, consequent, alternative };
 }
 
 // export function shortIfStatement(condition, consequent) {
@@ -47,72 +51,72 @@ export function ifStatement(condition, consequent, alternative) {
 // }
 
 export function lambdaDeclaration(lambda, initializer) {
-  return { kind: "LambdaStatement", lambda, initializer }
+  return { kind: "LambdaStatement", lambda, initializer };
 }
 
 export function lambda(name, params, body, type) {
-  return { kind: "Lambda", name, params, body, type}
+  return { kind: "Lambda", name, params, body, type };
 }
 
 export function functionDeclaration(fun) {
-  return { kind: "FunctionEvoke", fun }
+  return { kind: "FunctionEvoke", fun };
 }
 
 export function fun(name, parameters, body, returnHint) {
-  return { kind: "Function", name, parameters, body, returnHint }
+  return { kind: "Function", name, parameters, body, returnHint };
 }
 
 export function importedFunction(module, name, type) {
-  return { kind: "Function", module, name, type, imported: true }
+  return { kind: "Function", module, name, type, imported: true };
 }
 
 export function functionType(paramTypes, returnTypes) {
-  return { kind: "FunctionType", paramTypes, returnTypes}
+  return { kind: "FunctionType", paramTypes, returnTypes };
 }
 
 export function optionalType(baseType) {
-  return { kind: "OptionalType", baseType}
+  return { kind: "OptionalType", baseType };
 }
 
 export function returnStatement(expression) {
-  return { kind: "ReturnStatement", expression }
+  return { kind: "ReturnStatement", expression };
 }
 
 export function increment(variable) {
-  return { kind: "Increment", variable }
+  return { kind: "Increment", variable };
 }
 
 export function decrement(variable) {
-  return { kind: "Decrement", variable }
+  return { kind: "Decrement", variable };
 }
 
 export function assignment(target, source) {
-  return { kind: "Assignment", target, source}
+  return { kind: "Assignment", target, source };
 }
 
-export const breakStatement = { kind: "BreakStatement" }
+export const breakStatement = { kind: "BreakStatement" };
 
 export function binary(op, left, right, type) {
-  return { kind: "BinaryExpression", op, left, right, type}
+  return { kind: "BinaryExpression", op, left, right, type };
 }
 
 export function unary(op, operand, type) {
-  return { kind: "UnaryExpression", op, operand, type}
+  return { kind: "UnaryExpression", op, operand, type };
 }
 
 export function subscript(array, index) {
-  return { kind: "SubscriptExpression", array, index}
+  return { kind: "SubscriptExpression", array, index };
 }
 
 export function arrayExpression(elements) {
-  return { kind: "ArrayExpression", elements}
+  return { kind: "ArrayExpression", elements };
 }
 
 export function functionCall(callee, args) {
   if (callee.imported) {
     // NMK: idk smth that makes it act normal
   }
-  return { kind: "FunctionCall", callee, args, type: callee.type.returnTypes}
+  return { kind: "FunctionCall", callee, args, type: callee.type.returnTypes };
 }
 
 // NMK: Don't know if we actually need these, Toal's suggestion for his lang
@@ -123,31 +127,30 @@ export function functionCall(callee, args) {
 // const anyToVoidType = functionType([anyType], voidType)
 
 // NMK: Need to modify this to support our tomes (modules)
-// export const standardLibrary = Object.freeze({
-//   int: intType,
-//   float: floatType,
-//   boolean: boolType,
-//   string: stringType,
-//   void: voidType,
-//   any: anyType,
-//   π: variable("π", true, floatType),
-//   print: fun("print", anyToVoidType),
-//   sin: fun("sin", floatToFloatType),
-//   cos: fun("cos", floatToFloatType),
-//   exp: fun("exp", floatToFloatType),
-//   ln: fun("ln", floatToFloatType),
-//   hypot: fun("hypot", floatFloatToFloatType),
-//   bytes: fun("bytes", stringToIntsType),
-//   codepoints: fun("codepoints", stringToIntsType),
-// })
-
+export const standardLibrary = Object.freeze({
+  //   int: intType,
+  //   float: floatType,
+  //   boolean: boolType,
+  //   string: stringType,
+  //   void: voidType,
+  //   any: anyType,
+  //   π: variable("π", true, floatType),
+  //   print: fun("print", anyToVoidType),
+  //   sin: fun("sin", floatToFloatType),
+  //   cos: fun("cos", floatToFloatType),
+  //   exp: fun("exp", floatToFloatType),
+  //   ln: fun("ln", floatToFloatType),
+  //   hypot: fun("hypot", floatFloatToFloatType),
+  //   bytes: fun("bytes", stringToIntsType),
+  //   codepoints: fun("codepoints", stringToIntsType),
+});
 
 // NMK: Again, Don't know if we need Toal's monkey patching but wdik
 // We want every expression to have a type property. But we aren't creating
 // special entities for numbers, strings, and booleans; instead, we are
 // just using JavaScript values for those. Fortunately we can monkey patch
 // the JS classes for these to give us what we want.
-String.prototype.type = stringType
-Number.prototype.type = floatType
-BigInt.prototype.type = intType
-Boolean.prototype.type = boolType
+String.prototype.type = stringType;
+Number.prototype.type = floatType;
+BigInt.prototype.type = intType;
+Boolean.prototype.type = booleanType;
